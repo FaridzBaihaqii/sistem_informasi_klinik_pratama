9e96b97336651f8fd02f42d1337502e9c395cf96
HesaRayaUntara <rayirayiamira37@gmail.com>
2023-10-20 16:11:50 +0700

    menambahkan delete pada sidebar

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -187,7 +187,7 @@
                 <br>
                 <br>
                 <li class="nav-item">
-                    <a class="logout" href="{{ url('logout') }}">Logout</a>
+                    <a class="logout btn btn-outline-danger" style="color:white; background-color:red; width: 100px; margin-left:20px; padding-right:90px" href="{{ url('logout') }}">Logout</a>
                 </li>
                 
             </ul>
@@ -224,7 +224,7 @@
 
     <div class="container">
         <br/>
-        <a href="#menu-toggle" class="btn btn-default" id="menu-toggle">Toggle Menu</a>
+        <a href="#menu-toggle" class="btn btn-default" id="menu-toggle">▶ </a>
         @include('layout.flash-message')
         @yield('content')
        <br/>
