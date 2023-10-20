647c21439affcced1566bcafad36435999aee197
FaridzBaihaqii <muhammadfaridzbaihaqii@gmail.com>
2023-10-20 17:49:12 +0700

    update baru

---
+++ resources/views/pasien/edit.blade.php
@@ -0,0 +1,58 @@
+@extends('layout.layout')
+@section('title', 'Edit Pasien ')
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <span class="h1">
+                        Edit Data Pasien
+                    </span>
+                </div>
+                <div class="card-body">
+                    <form method="POST" action="simpan" enctype="multipart/form-data">
+                        <div class="row">
+                            <div class="col-md-5">
+                                <div class="form-group">
+                                    <label>Nama Pasien</label>
+                                    <input type="text" class="form-control" name="nama_pasien" value="{{ $pasien->nama_pasien }}" />
+                                </div>
+                                <label>Jenkel</label><br>
+                                <select name="jenkel" class="form-control" value="{{ $pasien->jenkel }}">
+                                    <option value="laki-laki">Laki-Laki</option>
+                                    <option value="perempuan">Perempuan</option>
+                                </select>
+                                <div class="form-group">
+                                    <label>Tanggal Lahir</label>
+                                    <input type="date" class="form-control" name="tgl_lahir" value="{{ $pasien->tgl_lahir }}" />
+                                </div>
+                                <div class="form-group">
+                                    <label>Alamat</label>
+                                    <input type="text" class="form-control" name="alamat" value="{{ $pasien->alamat }}" />
+                                </div>
+                                <div class="form-group">
+                                    <label>No Telp</label>
+                                    <input type="number" class="form-control" name="no_telp" value="{{ $pasien->no_telp }}" />
+                                </div>
+                                <div class="form-group">
+                                    <label>No BPJS</label>
+                                    <input type="number" class="form-control" name="no_bpjs" value="{{ $pasien->no_bpjs }}" />
+                                </div>
+                                <!-- <div class="form-group">
+                                    <input type="hidden" class="form-control" name="id_" value="{{ $pasien->id_ }}"/>
+                                </div> -->
+                                </div>
+                                @csrf
+                                <div class="d-flex mt-3">
+                                    <button type="submit" class="btn btn-primary" style="margin-right: 4px;">SIMPAN</button>
+                                    <a href="#" onclick="window.history.back();" class="btn btn-success">KEMBALI</a>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,10 @@
 use App\Http\Controllers\AkunController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-<<<<<<< HEAD
 use App\Http\Controllers\ResepsionisController;
 use App\Http\Controllers\PasienController;
-=======
-use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ApotekerController;
 use App\Http\Controllers\RekamMedisController;
->>>>>>> 9e96b97336651f8fd02f42d1337502e9c395cf96
-use App\Models\Resepsionis;
-use App\Http\Middleware\UserAccess;
 
 /*
 |--------------------------------------------------------------------------
@@ -31,24 +25,12 @@ Route::middleware(['guest'])->group(function () {
     Route::post('/', [AuthController::class, 'login']);
 });
 
-
-
 // Jika sudah login, kembali ke dalam halaman 
 Route::get('/home', function () {
     return redirect('dashboard/pasien');
 });
 
 // Route::middleware(['auth'])->group(function () {
-
-    //Pasien
-    Route::prefix('dashboard')->group(function () {
-        Route::get('/pasien', [PasienController::class, 'index']);
-        Route::get('/pasien/tambah', [PasienController::class, 'create']);
-        Route::post('/pasien/simpan', [PasienController::class, 'store']);
-        Route::get('/pasien/edit/{id}', [PasienController::class, 'edit']);
-        Route::post('/pasien/edit/simpan', [PasienController::class, 'update']);
-        Route::delete('/pasien/hapus', [PasienController::class, 'destroy']);
-    });
 
     //Asisten
     Route::prefix('admin')->group(function () {
@@ -108,12 +90,6 @@ Route::get('/home', function () {
 
     Route::get('/logout', [AuthController::class, 'logout']);
 
-<<<<<<< HEAD
-=======
-// });
-
-
->>>>>>> 9e96b97336651f8fd02f42d1337502e9c395cf96
 Route::prefix('auth')->group(function(){
     Route::get('/',[AuthController::class, 'index']);
     Route::get('/logout',[AuthController::class, 'logout']);
@@ -130,8 +106,3 @@ Route::prefix('dashboard')->group(function () {
     Route::post('/akun/edit/simpan', [AkunController::class, 'update']);
     Route::delete('/akun/hapus', [AkunController::class, 'destroy']);
 });
-<<<<<<< HEAD
-=======
-
-
->>>>>>> 9e96b97336651f8fd02f42d1337502e9c395cf96
